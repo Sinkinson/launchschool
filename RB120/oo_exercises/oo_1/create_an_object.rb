d8ee91ca16cdbc2bd192e6e47229c4ba53302d0b
Marcus Sinkinson <sinkinsonmarcus@gmail.com>
2021-08-14 08:38:26 +0200

    oo1 questions

---
+++ RB120/oo_exercises/oo_1/create_an_object.rb
@@ -0,0 +1,8 @@
+# Using the Cat class create an object (instance of the class)
+class Cat
+end
+
+kitty = Cat.new
+
+# We use the class method ::new to instantiate a new object
+# To make use of the Cat object we assign it to a variable
---
+++ RB120/oo_exercises/oo_1/what_are_you.rb
@@ -0,0 +1,9 @@
+# Using the code from create_an_object, add an initialize method that prints "I'm a cat!" when a new Cat object is initialized
+
+class Cat
+  def initialize
+    puts "I'm a cat!"
+  end
+end
+
+kitty = Cat.new
