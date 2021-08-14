5e0898b854a96d443c50fee00a62a252a288a7c3
Marcus Sinkinson <sinkinsonmarcus@gmail.com>
2021-08-14 08:31:42 +0200

    create a class

---
+++ RB120/oo_exercises/oo_1/create_a_class.rb
@@ -0,0 +1,7 @@
+# Create an empty class
+class Cat 
+end
+
+# Defining a class is similar to defining a method, there are just two main differences:
+# - class is used instead of def
+# - CamelCase is used when naming a class
