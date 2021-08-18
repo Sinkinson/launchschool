d1680ec340ae73717fc87b45ab5579cd06e6fd0e
Marcus Sinkinson <sinkinsonmarcus@gmail.com>
2021-08-18 07:34:33 +0200

    accessor exercises

---
+++ RB120/oo_exercises/oo_accessor_methods/formatting.rb
@@ -0,0 +1,21 @@
+# Using the following code, add the appropriate accessor methods so that @name is capitalized upon assignment
+
+class Person
+end
+
+person1 = Person.new
+person1.name = 'eLiZaBeTh'
+puts person1.name
+
+# Expected output: -> Elizabeth
+
+# solution
+class Person
+  attr_reader :name
+
+  def name=(name)
+    @name = name.capitalize
+  end
+end
+
+# If you are required to add extra functionality to a getter or setter method then you will need to implement your own
